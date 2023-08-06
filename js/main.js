db8b716fdcf77bdb19dd443631cff83446dd6b32
Sherzod-Tohirov <sherzodtohirov862@gmail.com>
2023-08-06 13:40:22 +0500

    Add filter to file

--- js/main.js
+++ js/main.js
@@ -1,50 +1,74 @@
 let elList = document.querySelector('.js-list');
-pokemons.forEach(item => {
+let elFilterInp = document.querySelector('.js-filter-input');
 
-elList.innerHTML +=	`<li class="list__item">
-					<img class="list__img" src="${item.img}" width="150" height="150" alt="${item.name} pokemon">
-					<div class="list__data">
-						<div class="list__l-data">
-							<p class="list__data-title">
-							    Name:
-								<span class="list__data-desc">${item.name}</span>
-							</p>
-							<p class="list__data-title">
-							    Type: 
-								<span class="list__data-desc">${item.type[0] ? item.type[0] : "ma'lum emas" }</span>
-							</p>
-							<p class="list__data-title">
-								Candy: 
-								<span class="list__data-desc">${item.candy}</span>
-							</p>
-							<p class="list__data-title">
-								Candy count: 
-								<span class="list__data-desc">${item.candy_count}</span>
-							</p>
-						</div>
-						<div class="list__r-data">
-							<p class="list__data-title">
-							   Height: 
-								<span class="list__data-desc">${item.height}</span>
-							</p>
-							<p class="list__data-title">
-							   Weight: 
-								<span class="list__data-desc">${item.weight}</span>
-							</p>
-							<p class="list__data-title">
-								Egg: 
-								<span class="list__data-desc">${item.egg}</span>
-							</p>
-							<p class="list__data-title">
-								Spawn time: 
-								<span class="list__data-desc">${item.spawn_time}</span>
-							</p>
-						</div>
-					</div>
-				</li>`
+elFilterInp.addEventListener('keyup', (evt) => {
+  let inpVal = elFilterInp.value.trim().toLowerCase();
+  
+  let result = pokemons.filter(item => {
+       return item.name.toLowerCase().startsWith(inpVal); 
+  });
+
+  renderPokemons(result);
 
 });
 
+renderPokemons(pokemons);
+
+
+
+
+
+
+
+function renderPokemons(arr) {
+	elList.innerHTML = '';
+	arr.forEach(item => {
+
+	elList.innerHTML +=	`<li class="list__item">
+						<img class="list__img" src="${item.img}" width="150" height="150" alt="${item.name} pokemon">
+						<div class="list__data">
+							<div class="list__l-data">
+								<p class="list__data-title">
+								    Name:
+									<span class="list__data-desc">${item.name}</span>
+								</p>
+								<p class="list__data-title">
+								    Type: 
+									<span class="list__data-desc">${item.type[0] ? item.type[0] : "ma'lum emas" }</span>
+								</p>
+								<p class="list__data-title">
+									Candy: 
+									<span class="list__data-desc">${item.candy}</span>
+								</p>
+								<p class="list__data-title">
+									Candy count: 
+									<span class="list__data-desc">${item.candy_count}</span>
+								</p>
+							</div>
+							<div class="list__r-data">
+								<p class="list__data-title">
+								   Height: 
+									<span class="list__data-desc">${item.height}</span>
+								</p>
+								<p class="list__data-title">
+								   Weight: 
+									<span class="list__data-desc">${item.weight}</span>
+								</p>
+								<p class="list__data-title">
+									Egg: 
+									<span class="list__data-desc">${item.egg}</span>
+								</p>
+								<p class="list__data-title">
+									Spawn time: 
+									<span class="list__data-desc">${item.spawn_time}</span>
+								</p>
+							</div>
+						</div>
+					</li>`
+
+	});
+
+}
 
 // pokemons.forEach(item => {
 // let elItem = document.createElement('li');
